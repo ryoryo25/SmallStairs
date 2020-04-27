f0503b3531778e7a96118715763685fe8b522279
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-04-28 02:38:55 +0900

    add legacy codes and exclude it from jar file

---
+++ src/main/java/ryoryo/smallstairs/block/LegacyCodes.java
@@ -0,0 +1,417 @@
+package ryoryo.smallstairs.block;
+
+public class LegacyCodes
+{
+	//************************************************************************************************************************
+//	/**
+//	 * B: ... T: xxx
+//	 * B: ... T: xxx
+//	 * B: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_TOP_NORTH = new AxisAlignedBB(base00, base06, base00, base10, base10, base03);
+//	/**
+//	 * B: ... T: ...
+//	 * B: ... T: xxx
+//	 * B: ... T: xxx
+//	 */
+//	protected static final AxisAlignedBB AABB_TOP_SOUTH = new AxisAlignedBB(base00, base06, base03, base10, base10, base10);
+//	/**
+//	 * B: ... T: xx.
+//	 * B: ... T: xx.
+//	 * B: ... T: xx.
+//	 */
+//	protected static final AxisAlignedBB AABB_TOP_WEST = new AxisAlignedBB(base00, base06, base00, base06, base10, base10);
+//	/**
+//	 * B: ... T: .xx
+//	 * B: ... T: .xx
+//	 * B: ... T: .xx
+//	 */
+//	protected static final AxisAlignedBB AABB_TOP_EAST = new AxisAlignedBB(base03, base06, base00, base10, base10, base10);
+
+
+//	/**
+//	 * B: ... T: ...
+//	 * B: xxx T: ...
+//	 * B: xxx T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_BOTTOM_SOUTH = new AxisAlignedBB(base00, base00, base03, base10, base03, base10);
+//	/**
+//	 * B: xx. T: ...
+//	 * B: xx. T: ...
+//	 * B: xx. T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_BOTTOM_WEST = new AxisAlignedBB(base00, base00, base00, base06, base03, base10);
+//	/**
+//	 * B: .xx T: ...
+//	 * B: .xx T: ...
+//	 * B: .xx T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_BOTTOM_EAST = new AxisAlignedBB(base03, base00, base00, base10, base03, base10);
+
+
+//	/**
+//	 * B: ... T: ...
+//	 * B: ... T: ...
+//	 * B: xxx T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_QTR_SOUTH = new AxisAlignedBB(base00, base03, base06, base10, base06, base10);
+//	/**
+//	 * B: x.. T: ...
+//	 * B: x.. T: ...
+//	 * B: x.. T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_QTR_WEST = new AxisAlignedBB(base00, base03, base00, base03, base06, base10);
+//	/**
+//	 * B: ..x T: ...
+//	 * B: ..x T: ...
+//	 * B: ..x T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_QTR_EAST = new AxisAlignedBB(base06, base03, base00, base10, base06, base10);
+
+
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: xxx M: xxx T: ...
+//	 * B: xxx M: xxx T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_BOT_SOUTH = new AxisAlignedBB(base00, base00, base03, base10, base06, base10);
+//	/**
+//	 * B: .xx M: .xx T: ...
+//	 * B: .xx M: .xx T: ...
+//	 * B: .xx M: .xx T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_BOT_WEST = new AxisAlignedBB(base00, base00, base00, base06, base06, base10);
+//	/**
+//	 * B: xx. M: xx. T: ...
+//	 * B: xx. M: xx. T: ...
+//	 * B: xx. M: xx. T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_BOT_EAST = new AxisAlignedBB(base03, base00, base00, base10, base06, base10);
+//	//**********************************************************************************************************************************
+//	/**
+//	 * B: ... M: xxx T: xxx
+//	 * B: ... M: xxx T: xxx
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_NORTH = new AxisAlignedBB(base00, base03, base00, base10, base10, base06);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: xxx T: xxx
+//	 * B: ... M: xxx T: xxx
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_SOUTH = new AxisAlignedBB(base00, base03, base03, base10, base10, base10);
+//	/**
+//	 * B: ... M: .xx T: .xx
+//	 * B: ... M: .xx T: .xx
+//	 * B: ... M: .xx T: .xx
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_WEST = new AxisAlignedBB(base00, base03, base00, base06, base10, base10);
+//	/**
+//	 * B: ... M: xx. T: xx.
+//	 * B: ... M: xx. T: xx.
+//	 * B: ... M: xx. T: xx.
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_EAST = new AxisAlignedBB(base03, base03, base00, base10, base10, base10);
+
+
+	//*********************************************************************************************************************************
+//	/**
+//	 * B: ... M: xx. T: xx.
+//	 * B: ... M: xx. T: xx.
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_OUTER_NW = new AxisAlignedBB(base00, base03, base00, base06, base10, base06);
+//	/**
+//	 * B: ... M: .xx T: .xx
+//	 * B: ... M: .xx T: .xx
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_OUTER_NE = new AxisAlignedBB(base03, base03, base00, base10, base10, base06);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: xx. T: xx.
+//	 * B: ... M: xx. T: xx.
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_OUTER_SW = new AxisAlignedBB(base00, base03, base03, base06, base10, base10);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: .xx T: .xx
+//	 * B: ... M: .xx T: .xx
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_OUTER_SE = new AxisAlignedBB(base03, base03, base03, base10, base10, base10);
+
+
+//	/**
+//	 * B: .xx M: .xx T: ...
+//	 * B: .xx M: .xx T: ...
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_BOT_OUTER_NE = new AxisAlignedBB(base03, base00, base00, base10, base06, base06);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: xx. M: xx. T: ...
+//	 * B: xx. M: xx. T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_BOT_OUTER_SW = new AxisAlignedBB(base00, base00, base03, base06, base06, base10);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: .xx M: .xx T: ...
+//	 * B: .xx M: .xx T: ...
+//	 */
+//	protected static final AxisAlignedBB SELECT_BOT_OUTER_SE = new AxisAlignedBB(base03, base00, base03, base10, base06, base10);
+
+
+//	/**
+//	 * B: ... M: xxx T: xxx
+//	 * B: ... M: xxx T: xxx
+//	 * B: ... M: xxx T: xxx
+//	 */
+//	protected static final AxisAlignedBB SELECT_TOP_INNER = new AxisAlignedBB(base00, base03, base00, base10, base10, base10);
+	//*********************************************************************************************************************************
+
+
+	//*************************************************************************************************************************************
+//	/**
+//	 * B: ... M: ... T: xx.
+//	 * B: ... M: ... T: xx.
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_TOP_OCT_NW = new AxisAlignedBB(base00, base06, base00, base06, base10, base06);
+//	/**
+//	 * B: ... M: ... T: .xx
+//	 * B: ... M: ... T: .xx
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_TOP_OCT_NE = new AxisAlignedBB(base03, base06, base00, base10, base10, base06);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: xx.
+//	 * B: ... M: ... T: xx.
+//	 */
+//	protected static final AxisAlignedBB AABB_TOP_OCT_SW = new AxisAlignedBB(base00, base06, base03, base06, base10, base10);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: .xx
+//	 * B: ... M: ... T: .xx
+//	 */
+//	protected static final AxisAlignedBB AABB_TOP_OCT_SE = new AxisAlignedBB(base03, base06, base03, base10, base10, base10);
+
+
+//	/**
+//	 * B: .xx M: ... T: ...
+//	 * B: .xx M: ... T: ...
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_BOT_OCT_NE = new AxisAlignedBB(base03, base00, base00, base10, base03, base06);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: xx. M: ... T: ...
+//	 * B: xx. M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_BOT_OCT_SW = new AxisAlignedBB(base00, base00, base03, base06, base03, base10);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: .xx M: ... T: ...
+//	 * B: .xx M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_BOT_OCT_SE = new AxisAlignedBB(base03, base00, base03, base10, base03, base10);
+
+
+//	/**
+//	 * B: ... M: ..x T: ...
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_OCT_NE = new AxisAlignedBB(base06, base03, base00, base10, base06, base03);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: x.. T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_OCT_SW = new AxisAlignedBB(base00, base03, base06, base03, base06, base10);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ..x T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_OCT_SE = new AxisAlignedBB(base06, base03, base06, base10, base06, base10);
+
+
+//**************************************************************************************************************************************
+//	/**
+//	 * B: ... M: xx. T: ...
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_QRT_NW = new AxisAlignedBB(base00, base03, base00, base06, base06, base03);
+//	/**
+//	 * B: ... M: .xx T: ...
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_QRT_NE = new AxisAlignedBB(base03, base03, base00, base10, base06, base03);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: xx. T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_QRT_SW = new AxisAlignedBB(base00, base03, base06, base06, base06, base10);
+//	/**
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: ... T: ...
+//	 * B: ... M: .xx T: ...
+//	 */
+//	protected static final AxisAlignedBB AABB_QRT_SE = new AxisAlignedBB(base03, base03, base06, base10, base06, base10);
+
+
+//	private static final Table<EnumFacing, EnumShape, EnumMap<EnumHalf, List<AxisAlignedBB>>> COLLISION_AABBS = HashBasedTable.create();
+//	private static final Table<EnumFacing, EnumShape, EnumMap<EnumHalf, AxisAlignedBB>> SELECTION_AABBS = HashBasedTable.create();
+//
+//	static
+//	{
+//		long startTime = System.nanoTime();
+//
+//		Stream.of(EnumFacing.values())
+//		.filter(f -> (f != EnumFacing.UP && f != EnumFacing.DOWN))
+//		.forEach(facing ->
+//		{
+//			for(EnumShape shape : EnumShape.values())
+//			{
+//				EnumMap<EnumHalf, List<AxisAlignedBB>> halfMap = new EnumMap<EnumHalf, List<AxisAlignedBB>>(EnumHalf.class);
+//
+//				for(EnumHalf half : EnumHalf.values())
+//				{
+//					List<AxisAlignedBB> bounds = Lists.newArrayList();
+//
+//					if(shape == EnumShape.STRAIGHT || shape == EnumShape.INNER_LEFT || shape == EnumShape.INNER_RIGHT)
+//					{
+//						bounds.add(getStraightStep1(facing, half));
+//						bounds.add(getStraightStep2(facing));
+//					}
+//
+//					if(shape == EnumShape.INNER_LEFT || shape == EnumShape.INNER_RIGHT)
+//					{
+//						bounds.add(getOuterStep1(facing, shape, half));
+//						bounds.add(getInnerStep2(facing, shape));
+//					}
+//
+//					if(shape == EnumShape.OUTER_LEFT || shape == EnumShape.OUTER_RIGHT)
+//					{
+//						bounds.add(getOuterStep1(facing, shape, half));
+//						bounds.add(getOuterStep2(facing, shape));
+//					}
+//
+//					halfMap.put(half, bounds);
+//				}
+//
+//				COLLISION_AABBS.put(facing, shape, halfMap);
+//			}
+//		});
+//
+//		Stream.of(EnumFacing.values())
+//		.filter(f -> (f != EnumFacing.UP && f != EnumFacing.DOWN))
+//		.forEach(facing ->
+//		{
+//			for(EnumShape shape : EnumShape.values())
+//			{
+//				EnumMap<EnumHalf, AxisAlignedBB> halfMap = new EnumMap<EnumHalf, AxisAlignedBB>(EnumHalf.class);
+//
+//				for(EnumHalf half : EnumHalf.values())
+//				{
+//					EnumAxis axis = half == EnumHalf.TOP ? EnumAxis.Y : EnumAxis.NONE;
+//					AxisAlignedBB outerBase = shape == EnumShape.OUTER_LEFT ? SELECT_OUTER : Utils.flipAABB(SELECT_OUTER, EnumAxis.X);
+//
+//					switch(shape)
+//					{
+//						default:
+//						case STRAIGHT:
+//							halfMap.put(half, Utils.rotateAABB(Utils.flipAABB(SELECT_STRAIGHT, axis),
+//									Utils.getRotationFromNorth(facing)));
+//							break;
+//						case OUTER_LEFT:
+//						case OUTER_RIGHT:
+//							halfMap.put(half, Utils.rotateAABB(Utils.flipAABB(outerBase, axis),
+//									Utils.getRotationFromNorth(facing)));
+//							break;
+//						case INNER_LEFT:
+//						case INNER_RIGHT:
+//							halfMap.put(half, Utils.flipAABB(SELECT_INNER, axis));
+//							break;
+//					}
+//				}
+//
+//				SELECTION_AABBS.put(facing, shape, halfMap);
+//			}
+//		});
+//
+//		long endTime = System.nanoTime();
+//		SmallStairs.LOGGER.info("Initializing collision boxes took " + (endTime-startTime)/1000000F + " ms");
+//	}
+
+
+//	private static List<AxisAlignedBB> getCollisionBoxList(IBlockState state)
+//	{
+//		List<AxisAlignedBB> list = COLLISION_AABBS.get(state.getValue(FACING), state.getValue(SHAPE)).get(state.getValue(HALF));
+//		return list != null ? list : Lists.newArrayList(FULL_BLOCK_AABB);
+//	}
+//
+//	private static AxisAlignedBB getSelectionBox(IBlockState state)
+//	{
+//		AxisAlignedBB aabb = SELECTION_AABBS.get(state.getValue(FACING), state.getValue(SHAPE)).get(state.getValue(HALF));
+//		return aabb != null ? aabb : FULL_BLOCK_AABB;
+//	}
+//
+//	/**
+//	 * use for EnumShape.STRAIGHT, EnumShape.INNER_LEFT and EnumShape.INNER_RIGHT
+//	 * @param state
+//	 * @return
+//	 */
+//	private static AxisAlignedBB getStraightStep1(EnumFacing facing, EnumHalf half)
+//	{
+//		return Utils.rotateAABB(Utils.flipYAABB(COLLISION_STRAIGHT_STEP1, half == EnumHalf.TOP),
+//				Utils.getRotationFromNorth(facing));
+//	}
+//
+//	/**
+//	 * use for EnumShape.STRAIGHT, EnumShape.INNER_LEFT and EnumShape.INNER_RIGHT
+//	 * @param state
+//	 * @return
+//	 */
+//	private static AxisAlignedBB getStraightStep2(EnumFacing facing)
+//	{
+//		return Utils.rotateAABB(COLLISION_STRAIGHT_STEP2, Utils.getRotationFromNorth(facing));
+//	}
+//
+//	/**
+//	 * use for EnumShape.INNER_LEFT and EnumShape.INNER_RIGHT
+//	 * @param state
+//	 * @return
+//	 */
+//	private static AxisAlignedBB getInnerStep2(EnumFacing facing, EnumShape shape)
+//	{
+//		return Utils.rotateAABB(COLLISION_STRAIGHT_STEP2,
+//				Utils.getRotationFromNorth(getRotateFacing(facing, shape)));
+//	}
+//
+//	/**
+//	 * use for EnumShape.OUTER_LEFT, EnumShape.OUTER_RIGHT, EnumShape.INNER_LEFT and EnumShape.INNER_RIGHT
+//	 * @param state
+//	 * @return
+//	 */
+//	private static AxisAlignedBB getOuterStep1(EnumFacing facing, EnumShape shape, EnumHalf half)
+//	{
+//		return Utils.rotateAABB(Utils.flipYAABB(COLLISION_OUTER_STEP1, half == EnumHalf.TOP),
+//				Utils.getRotationFromNorth(getRotateFacing(facing, shape)));
+//	}
+//
+//	/**
+//	 * use for EnumShape.OUTER_LEFT and EnumShape.OUTER_RIGHT
+//	 * @param state
+//	 * @return
+//	 */
+//	private static AxisAlignedBB getOuterStep2(EnumFacing facing, EnumShape shape)
+//	{
+//		return Utils.rotateAABB(COLLISION_OUTER_STEP2,
+//				Utils.getRotationFromNorth(getRotateFacing(facing, shape)));
+//	}
+}
